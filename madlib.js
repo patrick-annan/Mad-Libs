b9ae838fc8d07b2e419b1e61ae7483c9d81ab586
Patrick Annan <you@example.compatrickannan@hotmail.com>
2024-01-03 17:41:55 -0500

    updated inputs and css

---
+++ madlib.js
@@ -0,0 +1,21 @@
+
+createParagraph () {
+  var wordOne= 
+  var wordTwo=
+  var wordThree=
+  var wordFour=
+  var wordFive=
+  var wordSix=
+  var wordSeven=
+  var wordEight=
+  var wordNine=
+  var wordTen=
+  var wordEleven=
+  var wordTwelve=
+  var wordThirteen=
+  var wordFourteen=
+  var wordFifteen=
+  var wordSixteen=
+  var wordSeventeene=
+  var wordEighteen=
+}
