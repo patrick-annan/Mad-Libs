4df5d66d5161d317ba018ef41e71467cfdf80491
Patrick Annan <you@example.compatrickannan@hotmail.com>
2024-01-19 15:30:10 -0500

    added create paragraph and renamed js file to app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+
+createParagraph () {
+  var wordOne= document.getElementById().value;
+  var wordTwo= document.getElementById().value;
+  var wordThree= document.getElementById().value;
+  var wordFour= document.getElementById().value;
+  var wordFive= document.getElementById().value;
+  var wordSix= document.getElementById().value;
+  var wordSeven= document.getElementById().value;
+  var wordEight= document.getElementById().value;
+  var wordNine= document.getElementById().value;
+  var wordTen= document.getElementById().value;
+  var wordEleven= document.getElementById().value;
+  var wordTwelve= document.getElementById().value;
+  var wordThirteen= document.getElementById().value;
+  var wordFourteen= document.getElementById().value;
+  var wordFifteen= document.getElementById().value;
+  var wordSixteen= document.getElementById().value;
+  var wordSeventeene= document.getElementById().value;
+  var wordEighteen= document.getElementById().value;
+}
